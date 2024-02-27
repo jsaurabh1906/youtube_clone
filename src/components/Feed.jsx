2f85506fda8bf73297fbb4e00c612cba1fbdf496
saurabh <jsaurabh1906@gmail.com>
2024-02-27 10:57:20 +0530

    update: dynamic date at copyrights section

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -34,7 +34,7 @@ const Feed = () => {
           variant="body2"
           sx={{ mt: 1.5, color: "#fff" }}
         >
-          Copyright © 2022 JSM Media
+          Copyright © {new Date().getFullYear()} All rights reserved.
         </Typography>
       </Box>
 
